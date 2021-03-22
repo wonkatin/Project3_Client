03964318cedaafa77cd6d0706b12322022080bd5
wonkatin <j.drawers@gmail.com>
2021-03-22 15:30:07 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
+import Login from './components/auth/Login'
+import Profile from './components/auth/Profile'
+import Register from './components/auth/Register'
+import Trip from './components/trip-folder/Trip'
 
 function App() {
   return (
     <div className="App">
-      
+      <Welcome />
+      <Navbar />
+      <Login />
+      <Profile />
+      <Register />
+      <Trip />
     </div>
   );
 }
