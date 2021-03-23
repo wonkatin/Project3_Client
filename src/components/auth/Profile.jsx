c89f2c80cafd33e2e029ef86f77ad45e8e32bd55
wonkatin <j.drawers@gmail.com>
2021-03-22 21:49:57 -0500

    updated state and useEffect in app.js

--- src/components/auth/Profile.jsx
+++ src/components/auth/Profile.jsx
@@ -1,7 +1,10 @@
+import Trip from './components/trip-folder/Trip'
+
 export default function Profile() {
     return(
         <div>
            Hello from Profile  
+           <Trip />
         </div>
     )
 }
