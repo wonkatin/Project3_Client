61225590974a70b4f957b8d34b95ad5604a85422
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-03-23 11:44:55 -0700

    Start adding html to components

--- src/components/trip-folder/Trip.jsx
+++ src/components/trip-folder/Trip.jsx
@@ -1,7 +1,20 @@
+import { RouterBrowser as Router, Link } from 'react-router-dom'
+import TripCard from './TripCard.jsx'
+
 export default function Trips() {
     return(
-        <div>
-            Hello from Trips
+        <div class="background-trips"> {/* Need to add Router Tags */}
+            <h1>My Trips</h1>
+            <div class="upcoming-links">
+                <a href="">All Trips</a> {/* Need to add Link */}
+                <a href="">Upcoming Trips</a> {/* Need to add Link */}
+                <a href="">Past Trips</a> {/* Need to add Link */}
+            </div>
+            <div class="trip-container">
+                <TripCard /> {/* Need to map DB trips here as TripCards */}
+            </div>
         </div>
     )
 }
+
+
--- src/components/trip-folder/TripDetailContainer.jsx
+++
@@ -1,7 +0,0 @@
-export default function TripDetailContainer() {
-    return(
-        <div>
-            Hello from TripDetailContainer
-        </div>
-    )
-}
