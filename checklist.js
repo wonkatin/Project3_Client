9684d093c2385b4bf5d5174127ee3f48cc4c0506
wonkatin <j.drawers@gmail.com>
2021-03-24 22:55:00 -0500

    updated checklist data

---
+++ checklist.js
@@ -0,0 +1,366 @@
+[
+    { 
+      "itemName": "face mask",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "sleepwear",
+      "checked": false,
+      "category": "clothing and accessories"
+    },
+    {
+      "itemName": "underwear",
+      "checked": false,
+      "category": "clothing and accessories"
+    },
+    {
+      "itemName": "socks/stockings",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "undershirts/bras",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "pants",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "shorts",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "skirts",
+      "checked": false,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "dresses",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "t-shirts",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "dress shirts",
+      "checked": false  
+    },
+    {
+      "itemName": "sweaters/sweatshirts",
+      "checked": false,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "formal wear",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "swimsuit",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "coat/jacket/rainwear",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "hat",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "gloves",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "scarf",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "umbrella",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "shoes",
+      "checked": false,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "boots",
+      "checked": false ,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "sandals",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "belts",
+      "checked": false,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "ties",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "jewelry",
+      "checked": false,
+      "category": "clothing and accessories" 
+    },
+    {
+      "itemName": "sunglasses",
+      "checked": false,
+      "category": "clothing and accessories"  
+    },
+    {
+      "itemName": "toothbrush",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "toothpaste",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "dental floss",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "soap",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "shampoo",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "conditioner",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "deodorant",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "brush",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "hairstyling accessories",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "hairstyling tools",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "lotion",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "sunscreen",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "contact lenses/solution",
+      "checked": false,
+      "category": "toiletries"   
+    },
+    {
+      "itemName": "shaving supplies",
+      "checked": false,
+      "category": "toiletries"
+       
+    },
+    {
+      "itemName": "makeup and accessories",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "feminine-hygiene products",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "nail file/clippers",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "tweezers",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "hand sanitizer",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "first aid",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "medications",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "vitamins",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "insect repellent",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "lint roller",
+      "checked": false,
+      "category": "toiletries"  
+    },
+    {
+      "itemName": "phone",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "laptop",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "camera",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "headphones",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "electronic chargers",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "cash",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "credit cards",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "travel documents",
+      "checked": false  
+    },
+    {
+      "itemName": "voltage adaptors",
+      "checked": false,
+      "category": "miscellaneous" 
+    },
+    {
+      "itemName": "day bag, backpack or purses",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "snacks",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "journal",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "books",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "travel blanket",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "travel pillow",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "earplugs and eye mask",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "flashlight",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "batteries",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "binoculars",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "maps",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "camping gear",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "reusable water bottle",
+      "checked": false,
+      "category": "miscellaneous"  
+    },
+    {
+      "itemName": "games",
+      "checked": false,
+      "category": "miscellaneous"  
+    }
+  ]
