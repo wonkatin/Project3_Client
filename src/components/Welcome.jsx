a148348bb08e8eeb04f814106fbf18a4737dbac1
wonkatin <j.drawers@gmail.com>
2021-03-26 08:50:17 -0500

    able to see checklist WOOT

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import img from '../img/App-logo.png'
 
 
--- src/components/trip-folder/TripDetail.jsx
+++ src/components/trip-folder/TripDetail.jsx
@@ -1,4 +1,4 @@
-// import ChecklistTool from '../tools/ChecklistTool'
+import ChecklistTool from '../tools/ChecklistTool'
 // import ExpenseTool from '../tools/ExpenseTool'
 // import FlightTool from '../tools/FlightTool'
 // import LodgingTool from '../tools/LodgingTool'
@@ -6,6 +6,7 @@
 // import ScheduleTool from '../tools/ScheduleTool'
 
 export default function TripDetail(props) {
+    console.log(props)
     return(
         <div>
             <h1>Hello from TripDetail</h1>
@@ -13,15 +14,15 @@ export default function TripDetail(props) {
             <div className="detail-container">
                 
                 <div className="detail-header">
-                    {/* <h1>{props.name}</h1>
-                    <h4>{props.location}</h4>
-                    <h5>{props.fromDate}</h5> */}
-                    {/* <img src={props.img} alt={props.location}/> */}
+                    <h1>{props.currentUser.trips}</h1>
+                    
+                   
+                   
                 </div>
                 
                 <div className="tool-container">
-                    {/* <ChecklistTool className="tool"/>
-                    <ExpenseTool className="tool"/>
+                    <ChecklistTool className="tool"/>
+                    {/* <ExpenseTool className="tool"/>
                     <FlightTool className="tool"/>
                     <LodgingTool className="tool"/>
                     <NoteTool className="tool"/>
